4d559b256dd40e6ac76c15d2dcf8f86775c7cc34
Tomohiro Hagino <marspeoplehg@gmail.com>
2019-12-15 13:13:37 +0900

    finish 2-8

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,6 @@
 class Goal < ApplicationRecord
   belongs_to :user
+  has_many :todos, -> { order("done ASC, position ASC").includes(:goal) }, dependent: :destroy
+
+  validates :title, presence: true
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,14 @@
 class Todo < ApplicationRecord
   belongs_to :goal
+  acts_as_list scope: [:goal_id]
+
+  validates :position, numericality: { only_integer: true, allow_nil: true }
+  validates :content, presence: true
+  validates :done, inclusion: { in: [true, false] }
+  after_initialize :set_default, if: :new_record?
+
+  private
+  def set_default
+      self.done = false if self.done.blank?
+  end
 end
